86a584fa8ba7ec157ecde1c6f0ba1b9119f37cad
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-10-27 09:34:04 -0600

    basic layout for calc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-6">Result:</div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <button class="btn btn-info">+</button>
+        <button class="btn btn-info">-</button>
+        <button class="btn btn-info">X</button>
+        <button class="btn btn-info">÷</button>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <button class="btn btn-info">7</button>
+        <button class="btn btn-info">8</button>
+        <button class="btn btn-info">9</button>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <button class="btn btn-info">4</button>
+        <button class="btn btn-info">5</button>
+        <button class="btn btn-info">6</button>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <button class="btn btn-info">1</button>
+        <button class="btn btn-info">2</button>
+        <button class="btn btn-info">3</button>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <button class="btn btn-info">0</button>
+        <button class="btn btn-info">.</button>
+        <button class="btn btn-info">C</button>
+        <button class="btn btn-info">=</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "calculator",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
